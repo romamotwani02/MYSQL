4a50471ce12244575752b763bf28c168ed792466
romamotwani02 <166395387+romamotwani02@users.noreply.github.com>
2024-09-03 21:44:58 +0530

    Update 7)Average_post_hiatus.sql

--- 7)Average_post_hiatus.sql
+++ 7)Average_post_hiatus.sql
@@ -4,3 +4,18 @@ FROM posts
 WHERE YEAR(post_date) = 2021
 GROUP BY user_id
 HAVING COUNT(post_id) > 1;
+
+
+
+
+
+SELECT
+    user_id,
+    DATEDIFF(
+        MAX(STR_TO_DATE(post_date, '%Y/%m/%d')),
+        MIN(STR_TO_DATE(post_date, '%Y/%m/%d'))
+    ) AS days_between_posts
+FROM posts
+WHERE STR_TO_DATE(post_date, '%Y/%m/%d') BETWEEN '2021-01-01' AND '2021-12-31'
+GROUP BY user_id
+HAVING COUNT(post_id) > 1;
